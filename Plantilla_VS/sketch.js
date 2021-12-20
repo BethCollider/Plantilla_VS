930395e440e5c51a69094656d6f6a6967664c668
BethCollider <88354512+BethCollider@users.noreply.github.com>
2021-12-20 17:34:58 -0600

    Add files via upload
    Archivos VS

---
+++ Plantilla_VS/sketch.js
@@ -0,0 +1,20 @@
+//Declarar variables con objetos
+
+//Funcion de configuracion
+function setup() {
+  //Diseño de area de juego
+  createCanvas(400,400);
+  //Diseñoi de objeto 
+ 
+
+}
+
+//Funcion de dibujo
+function draw() 
+{
+  //Establecer color de fondo
+  background(51);
+//Codigo para proyectar objetos
+
+}
+
